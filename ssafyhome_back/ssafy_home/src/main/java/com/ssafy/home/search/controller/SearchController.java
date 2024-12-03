008f7542ada91a8d81ea80d77697ca62e40f8c90
강인혁 <ggh2260@gmail.com>
2024-12-03 17:36:59 +0900

    지역검색, 주택검색 Full-Text Index 적용

--- ssafyhome_back/ssafy_home/src/main/java/com/ssafy/home/search/controller/SearchController.java
+++ ssafyhome_back/ssafy_home/src/main/java/com/ssafy/home/search/controller/SearchController.java
@@ -17,29 +17,40 @@ import com.ssafy.home.search.model.service.SearchService;
 @RestController
 @RequestMapping("/api/search")
 public class SearchController {
- private final SearchService searchService;
+	private final SearchService searchService;
 
- public SearchController(SearchService searchService) {
-     this.searchService = searchService;
- }
+	public SearchController(SearchService searchService) {
+		this.searchService = searchService;
+	}
 
- @GetMapping("/region")
- public ResponseEntity<?> searchRegion(@RequestParam String keyword) {
-     List<RegionDto> regions = searchService.searchRegion(keyword);
-     return ResponseEntity.ok(regions);
- }
+	@GetMapping("/region")
+	public ResponseEntity<?> searchRegion(@RequestParam String keyword) {
+		List<RegionDto> regions = searchService.searchRegion(keyword);
+		return ResponseEntity.ok(regions);
+	}
 
- @GetMapping("/house")
- public ResponseEntity<?> searchHouse(@RequestParam String keyword) {
-     List<HouseDto> houses = searchService.searchHouse(keyword);
-     return ResponseEntity.ok(houses);
- }
+	@GetMapping("/house")
+	public ResponseEntity<?> searchHouse(@RequestParam String keyword) {
+		List<HouseDto> houses = searchService.searchHouse(keyword);
+		return ResponseEntity.ok(houses);
+	}
 
- @GetMapping("/all")
- public ResponseEntity<?> searchAll(@RequestParam String keyword) {
-     Map<String, Object> result = new HashMap<>();
-     result.put("regions", searchService.searchRegion(keyword));
-     result.put("houses", searchService.searchHouse(keyword));
-     return ResponseEntity.ok(result);
- }
+	@GetMapping("/all")
+	public ResponseEntity<?> searchAll(@RequestParam String keyword) {
+		
+
+		Map<String, Object> result = new HashMap<>();
+		long startTime = System.currentTimeMillis(); // 시작 시간 기록
+		result.put("regions", searchService.searchRegion(keyword));
+		long endTime = System.currentTimeMillis(); // 끝 시간 기록
+		System.out.println("지역 검색 Execution time: " + (endTime - startTime) + " ms");
+		
+		startTime = System.currentTimeMillis(); // 시작 시간 기록
+		result.put("houses", searchService.searchHouse(keyword));
+		endTime = System.currentTimeMillis(); // 끝 시간 기록
+		System.out.println("주택 검색 Execution time: " + (endTime - startTime) + " ms");
+		
+
+		return ResponseEntity.ok(result);
+	}
 }
--- ssafyhome_back/ssafy_home/src/main/java/com/ssafy/home/search/model/service/SearchServiceImpl.java
+++ ssafyhome_back/ssafy_home/src/main/java/com/ssafy/home/search/model/service/SearchServiceImpl.java
@@ -1,6 +1,5 @@
 package com.ssafy.home.search.model.service;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,36 +12,47 @@ import com.ssafy.home.search.model.mapper.SearchMapper;
 
 @Service
 public class SearchServiceImpl implements SearchService {
- private final SearchMapper searchMapper;
+	private final SearchMapper searchMapper;
 
- public SearchServiceImpl(SearchMapper searchMapper) {
-     this.searchMapper = searchMapper;
- }
+	public SearchServiceImpl(SearchMapper searchMapper) {
+		this.searchMapper = searchMapper;
+	}
 
- @Override
- public List<RegionDto> searchRegion(String keyword) {
-     return searchMapper.searchRegion(keyword);
- }
+	// 지역검색 Service
+	@Override
+	public List<RegionDto> searchRegion(String keyword) {
+		String formattedKeyword = formattingKeyword(keyword);
+		return searchMapper.searchRegion(formattedKeyword);
+	}
 
- @Override
- public List<HouseDto> searchHouse(String keyword) {
-	 List<HouseDto> houses = searchMapper.searchHouse(keyword);
+	// 주택검색 Service
+	@Override
+	public List<HouseDto> searchHouse(String keyword) {
+		// 1. "서울" 검색 시 "서울"이라는 이름이 들어간 아파트, 빌라, 오피스텔 모두 가져오기 
+		String formattedKeyword = formattingKeyword(keyword);
+		List<HouseDto> houses = searchMapper.searchHouse(formattedKeyword);
 
-     // 각 HouseDto에 대해 dongcodes 테이블에서 정보를 가져와 설정
-     for (HouseDto house : houses) {
-         Map<String, String> params = new HashMap<>();
-         params.put("sggCd", house.getSggCd());
-         params.put("umdCd", house.getUmdCd());
+		// 2. 각 HouseDto에 시-구-동 정보를 조회해 설정
+		for (HouseDto house : houses) {		
+			// DB 조회용 파라미터 설정 
+			Map<String, String> params = new HashMap<>();
+			params.put("sggCd", house.getSggCd());
+			params.put("umdCd", house.getUmdCd());
 
-         // dongcodes에서 조회한 값 설정
-         Map<String, String> dongInfo = searchMapper.getDongInfo(params);
-         if (dongInfo != null) {
-             house.setSidoName(dongInfo.get("sidoName"));
-             house.setGugunName(dongInfo.get("gugunName"));
-             house.setDongName(dongInfo.get("dongName"));
-         }
-     }
+			// DB 조회
+			Map<String, String> dongInfo = searchMapper.getDongInfo(params);
+			if (dongInfo != null) {
+				house.setSidoName(dongInfo.get("sidoName"));
+				house.setGugunName(dongInfo.get("gugunName"));
+				house.setDongName(dongInfo.get("dongName"));
+			}
+		}
 
-     return houses;
- }
+		return houses;
+	}
+	
+	// Full-Text Index를 활용하기 위한 키워드 변경 
+	public String formattingKeyword(String keyword) {
+		return "+" + keyword + "*";
+	}
 }
